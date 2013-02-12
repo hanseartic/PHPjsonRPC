c54f311a6aaa00a6e80ef485bcdc0be71ae8a028
hanseartic <schuette.paul@gmail.com>
2013-02-12 16:07:28 +0100

    Add license information

--- RPCServer.php
+++ RPCServer.php
@@ -1,7 +1,20 @@
 <?php
-/**
+/*
  * RPCServer.php
  * Created: 12.02.13 09:31
+ * Copyright 2013 Paul Schütte
+ *
+ * This file is part of php-jsonRPC.
+ *
+ * php-jsonRPC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * php-jsonRPC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with php-jsonRPC. If not, see http://www.gnu.org/licenses/.
+ */
+
+/**
+ * Acts as an RPC-Server to serve existing methods over an RPC-style interface.
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 class RPCServer {
 
