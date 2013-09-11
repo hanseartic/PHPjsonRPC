472c0ad273e0e36570355a4afba47db4b78921ec
hanseartic <schuette.paul@gmail.com>
2013-09-12 00:46:00 +0200

    Changes license to MIT

--- RPCServer.php
+++ RPCServer.php
@@ -2,19 +2,35 @@
 /*
  * RPCServer.php
  * Created: 12.02.13 09:31
- * Copyright 2013 Paul Schütte
  *
  * This file is part of php-jsonRPC (https://github.com/hanseartic/PHPjsonRPC)
  *
- * php-jsonRPC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- * php-jsonRPC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with php-jsonRPC. If not, see http://www.gnu.org/licenses/.
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Paul Schütte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 /**
  * Acts as an RPC-Server to serve existing methods over an RPC-style interface.
  *
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
  */
 namespace PHPjsonRPC;
 class RPCServer {
